315f455cd6bc4c2ba6e9c5cba8479755b9a570d7
naldikt <naldikt@gmail.com>
2020-05-13 20:27:35 -0700

    Update CompiledMixin.swift

--- NXSS/Parser/Helpers/Compiled/CompiledMixin.swift
+++ NXSS/Parser/Helpers/Compiled/CompiledMixin.swift
@@ -3,7 +3,7 @@
 //  NXSS
 //
 //  Created by Nalditya Kusuma on 9/22/15.
-//  Copyright (c) 2015 Rhapsody. All rights reserved.
+//  Copyright (c) 2015 Nalditya Kusuma. All rights reserved.
 //
 
 import Foundation
